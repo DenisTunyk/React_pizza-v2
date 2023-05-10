44bc2b610acdb9c940e8dfe0c3dfdff2ca3488de
DenisTunyk <tundenis43@gmail.com>
2023-05-10 17:13:02 +0300

    lesson2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,28 @@
+import { Header } from './Header/Header';
+import { Categories } from './Categories/Categories';
+import { Sort } from './Sort/Sort';
+import { PizzaBlock } from './PizzaBlock/PizzaBlock';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div class="wrapper">
+      <Header />
+      <div class="content">
+        <div class="container">
+          <div class="content__top">
+            <Categories />
+            <Sort />
+          </div>
+          <h2 class="content__title">Все пиццы</h2>
+          <div class="content__items">
+            <PizzaBlock title="Мексиканская" price={500} />
+            <PizzaBlock title="Мексиканская" price={333} />
+            <PizzaBlock title="Мексиканская" price={111} />
+            <PizzaBlock title="Мексиканская" price={888} />
+            <PizzaBlock title="Мексиканская" price={999} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
