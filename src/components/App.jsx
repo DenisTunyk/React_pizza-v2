009d7441797e3034f41bfd07302612b10a9dfb6b
DenisTunyk <tundenis43@gmail.com>
2023-05-12 15:47:02 +0300

    fetch

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,9 +3,16 @@ import { Categories } from './Categories/Categories';
 import { Sort } from './Sort/Sort';
 import { PizzaBlock } from './PizzaBlock/PizzaBlock';
 import Pizza from '../assets/pizza.json';
+import React from 'react';
 
 export const App = () => {
-  console.log(Pizza);
+  //const [items, setItems] = React.useEffect([]);
+
+  React.useEffect(() => {
+    fetch('https://640734a477c1a905a0f16e16.mockapi.io/api/v1/:pizza').then(
+      res => res.json().then(json => console.log(json))
+    );
+  }, []);
   return (
     <div className="wrapper">
       <Header />
