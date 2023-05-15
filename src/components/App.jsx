fe384337ccb22e5b6d4c6a1c346f37c49f04077e
DenisTunyk <tundenis43@gmail.com>
2023-05-15 15:38:43 +0300

    les-7-dop

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,38 +1,16 @@
-import { Header } from './Header/Header';
-import { Categories } from './Categories/Categories';
-import { Sort } from './Sort/Sort';
-import { PizzaBlock } from './PizzaBlock/PizzaBlock';
-//import Pizza from '../assets/pizza.json';
 import React from 'react';
-import Sceleton from './PizzaBlock/Sceleton';
+
+import { Header } from './Header/Header';
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
 
 export const App = () => {
-  const [items, setItems] = React.useState([]);
-  const [isLoading, setIsLoading] = React.useState(true);
-
-  React.useEffect(() => {
-    fetch('https://640734a477c1a905a0f16e16.mockapi.io/api/v1/pizza')
-      .then(res => res.json())
-      .then(arr => {
-        setItems(arr);
-        setIsLoading(false);
-      });
-  }, []);
   return (
     <div className="wrapper">
       <Header />
       <div className="content">
         <div className="container">
-          <div className="content__top">
-            <Categories />
-            <Sort />
-          </div>
-          <h2 className="content__title">Все пиццы</h2>
-          <div className="content__items">
-            {isLoading
-              ? [...new Array(6)].map((_, index) => <Sceleton key={index} />)
-              : items.map(obj => <PizzaBlock key={obj.id} {...obj} />)}
-          </div>
+          <NotFound></NotFound>
         </div>
       </div>
     </div>
---
+++ src/components/NotFoundBlock/NotFoundBlock.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import styles from './NotFoundBlock.module.scss';
+
+export const NotFoundBlock = () => {
+  return (
+    <div className={styles.root}>
+      <h1>
+        <span>(:</span>
+        <br />
+        Ничего не найдено
+      </h1>
+      <p className={styles.description}>
+        К сожалению на этой странице нет никакого контента
+      </p>
+    </div>
+  );
+};
