50a755fc8b3fcc2c9ce307c5fa90ecc657d1a880
DenisTunyk <98811233+DenisTunyk@users.noreply.github.com>
2024-09-02 17:37:25 +0300

    useContext

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,11 @@
 import { Link } from 'react-router-dom';
+import React from 'react';
 
 import LogoSvg from '../../assets/img/pizza-logo.svg';
 
 import { Search } from '../Search/Search';
 
-export const Header = ({ searchValue, setSearchValue }) => {
+export const Header = () => {
   return (
     <div className="header">
       <div className="container">
@@ -17,7 +18,7 @@ export const Header = ({ searchValue, setSearchValue }) => {
             </div>
           </div>
         </Link>
-        <Search searchValue={searchValue} setSearchValue={setSearchValue} />
+        <Search />
         <div className="header__cart">
           <Link to="/cart" className="button button--cart">
             <span>520 ₽</span>
--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -1,7 +1,9 @@
 import React from 'react';
 import styles from './Search.module.scss';
+import { SearchContext } from 'components/App';
 
-export const Search = ({ searchValue, setSearchValue }) => {
+export const Search = () => {
+  const { searchValue, setSearchValue } = React.useContext(SearchContext);
   return (
     <div className={styles.root}>
       <svg className={styles.icon} viewBox="0 0 50 50">
