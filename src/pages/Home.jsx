cfe605735db3bcdd19caec347853cc6d6fd9ae6d
DenisTunyk <98811233+DenisTunyk@users.noreply.github.com>
2024-09-12 09:31:07 +0300

    fitch_bug

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,18 +1,27 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import axios from 'axios';
+import qs from 'qs';
 
-import { useContext } from 'react';
+import { useContext, useEffect } from 'react';
 import { Categories } from '../components/Categories/Categories';
 import { PizzaBlock } from '../components/PizzaBlock/PizzaBlock';
-import { Sort } from '../components/Sort/Sort';
+import { menu, Sort } from '../components/Sort/Sort';
 import Sceleton from '../components/PizzaBlock/Sceleton';
 import { Pagination } from '../components/Pagination/Pagination';
 import { useDispatch, useSelector } from 'react-redux';
-import { setCategoryId, setCurrentPage } from '../redux/slices/filterSlice';
+import {
+  setCategoryId,
+  setCurrentPage,
+  setFilters,
+} from '../redux/slices/filterSlice';
 import { SearchContext } from 'components/App';
+import { useNavigate } from 'react-router-dom';
 
 export const Home = () => {
   const { searchValue } = useContext(SearchContext);
+  const navigate = useNavigate();
+  const isSearch = useRef(false);
+  const isMounted = useRef(false);
 
   const dispatch = useDispatch();
   const categoryId = useSelector(state => state.filterReduce.categoryId);
@@ -33,17 +42,15 @@ export const Home = () => {
 
   const [items, setItems] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(true);
-  //const [categoryId, setCategoryId] = React.useState(0);
-  //const [curentPage, setCurentPage] = React.useState(1);
 
-  React.useEffect(() => {
+  const fetchPizzas = () => {
     setIsLoading(true);
     const order = sortType.sortProperty.includes('-') ? 'ask' : 'desc';
     const sortBy = sortType.sortProperty.replace('-', '');
     const category = categoryId > 0 ? `category=${categoryId}` : '';
     const search = searchValue ? `&search=${searchValue}` : '';
-    console.log('SearchValue = ', searchValue);
-    console.log('search = ', search);
+    // console.log('SearchValue = ', searchValue);
+    // console.log('search = ', search);
     // Запрос через fetch
     // fetch(
     //   `https://640734a477c1a905a0f16e16.mockapi.io/api/v1/pizza?page=${curentPage}&limit=4&${category}&sortBy=${sortBy}&order=${order}`
@@ -55,9 +62,9 @@ export const Home = () => {
     //   });
 
     //Запрос через axios
-    console.log(
-      `https://640734a477c1a905a0f16e16.mockapi.io/api/v1/pizza?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`
-    );
+    // console.log(
+    //   `https://640734a477c1a905a0f16e16.mockapi.io/api/v1/pizza?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`
+    // );
     axios
       .get(
         `https://640734a477c1a905a0f16e16.mockapi.io/api/v1/pizza?page=${currentPage}&limit=8&${category}&sortBy=${sortBy}&order=${order}${search}`
@@ -67,9 +74,37 @@ export const Home = () => {
         setItems(res.data);
         setIsLoading(false);
       });
+  };
 
+  //Если был первый рендер, то проверяем запросы qwery
+  useEffect(() => {
+    if (window.location.search) {
+      const params = qs.parse(window.location.search.substring(1));
+      const sort = menu.find(obj => obj.sortProperty === params.sortProperty);
+      dispatch(setFilters({ ...params, sort }));
+      isSearch.current = true;
+    }
+  }, []);
+
+  useEffect(() => {
     window.scrollTo(0, 0);
-  }, [categoryId, sortType, currentPage, searchValue]);
+    if (!isSearch.current) {
+      fetchPizzas();
+    }
+    isSearch.current = false;
+  }, [categoryId, sortType.sortProperty, currentPage, searchValue]);
+
+  useEffect(() => {
+    if (isMounted.current) {
+      const queryString = qs.stringify({
+        sortProperty: sortType.sortProperty,
+        categoryId,
+        currentPage,
+      });
+      navigate(`?${queryString}`);
+    }
+    isMounted.current = true;
+  }, [categoryId, sortType.sortProperty, currentPage, navigate]);
 
   let pizzas;
 
